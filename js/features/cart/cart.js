543df3040e327b361cb3f3f23b9f85b50ba03d3c
Nirali-sheta <niralisheta74@gmail.com>
2024-12-08 15:41:43 +0530

    responsive homepage added

--- js/features/cart/cart.js
+++ js/features/cart/cart.js
@@ -10,18 +10,19 @@ function displayProductInCart() {
     const totalMRP = document.getElementById('total-mrp');
     const totalAmount = document.getElementById('total-amt');
     const discount=document.getElementById('discount');
-    const couponValue = 10;
-    const taxValue = 50;
+    const couponDiscount=document.getElementById('coupon-discount');
+    const tax=document.getElementById('Tax');
+
 
     let totalMRPValue = 0;
     let totalDiscount = 0;
     let totalAmountValue = 0;
 
-    cart.forEach((product, index) => {
+    cart.forEach(product => {
         const cartItem = document.createElement('div');
         cartItem.classList.add('cart-item');
         cartItem.innerHTML = `
-            <input type="checkbox" class="select-item">
+            <input type="checkbox" class="select-item" checked>
             <img src="/html/src/${product.imageUrls[0]}" alt="${product.name}" class="product-image">
             <div class="product-details">
                 <h4 class="product-name">${product.name}</h4>
@@ -29,16 +30,18 @@ function displayProductInCart() {
                 <p class="product-size">Size: ${product.sizes[0]}</p>
                 <p class="product-color">Color: White</p>
                 <p class="product-price">&#8377;${product.price}</p>
+                
             </div>
-            <div class="product-quantity">
-                <button class="quantity-decrease">-</button>
-                <input type="text" value="1" class="quantity-input" min="1">
-                <button class="quantity-increase">+</button>
-            </div>
-            <div class="product-total">
+                <div class="product-quantity">  
+                    <button class="quantity-decrease">-</button>
+                    <input type="text" value="1" class="quantity-input" min="1">
+                    <button class="quantity-increase">+</button>
+                </div>
+                <div class="product-total">
                 <p>&#8377;${product.price}</p>
-            </div>
-        `;
+                </div>
+                <button class="remove-item">Remove</button>
+           `;
 
         // Append the cart item to the cart container
         cartContainer.appendChild(cartItem);
@@ -47,6 +50,7 @@ function displayProductInCart() {
         const totalPrice = cartItem.querySelector('.product-total');
         const decreaseBtn = cartItem.querySelector('.quantity-decrease');
         const increaseBtn = cartItem.querySelector('.quantity-increase');
+        const checkbox=cartItem.querySelector('.select-item');
 
         // update the total price for this product
         function updateProductTotal() {
@@ -72,21 +76,46 @@ function displayProductInCart() {
             let qty = parseInt(qtyInput.value);
             qtyInput.value = qty + 1;
             updateProductTotal();
-        });       
+        });      
+        
+        checkbox.addEventListener('change',updateTotalAmt);
+        const removebtn=cartItem.querySelector('.remove-item');
+        removebtn.addEventListener('click',()=>{
+            
+            let cart = JSON.parse(localStorage.getItem('cart')) || [];
+            cart=cart.filter(item=>item.id!==product.id);
+           
+
+            localStorage.setItem('cart',JSON.stringify(cart));
+            cartItem.remove();
+
+            if(cart.length===0){
+                document.body.innerHTML="<p>Your Cart is empty!!</p>";
+            };
+            updateTotalAmt();
+        });
     });
 
 
 function updateTotalAmt() {
     totalMRPValue = 0;  
-
+    let isSelected=false;
     // Calculate the total MRP for all products in the cart
-    const qtyInputs = cartContainer.querySelectorAll('.quantity-input');
-    qtyInputs.forEach((input, idx) => {
-        const productQty = parseInt(input.value);
-        if (productQty >= 1) {
+    const cartItems = cartContainer.querySelectorAll('.cart-item');
+    cartItems.forEach((item, idx) => {
+        const checkbox=item.querySelector('.select-item')
+        const qtyInput=item.querySelector('.quantity-input');
+        const productQty = parseInt(qtyInput.value);
+       
+        if (checkbox.checked &&productQty >= 1) {
+            isSelected=true;
             totalMRPValue += cart[idx].price * productQty;
         }
     });    
+
+   const couponValue=isSelected?10:0;
+   const taxValue=isSelected?50:0;
+
     // Calculate discount based on total MRP
     if (totalMRPValue >= 5000) {
         totalDiscount = totalMRPValue * 0.22;
@@ -100,11 +129,13 @@ function updateTotalAmt() {
 
     // Calculate the total amount
     totalAmountValue = totalMRPValue - totalDiscount - couponValue + taxValue;
-    console.log(totalAmountValue);
+
     // Update the displayed values
     totalMRP.innerHTML='&#8377;'+(totalMRPValue).toFixed(2);
     discount.innerHTML='&#8377;'+(totalDiscount).toFixed(2);
     totalAmount.innerHTML='&#8377;'+(totalAmountValue).toFixed(2);
+    couponDiscount.innerHTML='&#8377;'+couponValue;
+    tax.innerHTML='&#8377;'+taxValue;
 }
     updateTotalAmt();
 }
